401229e12348cd449661028dbfde132efdef6fa8
venilson1 <venilson2009@hotmail.com>
2022-01-15 20:56:36 -0300

    ORM mapping & database seeding

--- backend/src/main/java/com/devSuperior/dsCatalog/entities/Product.java
+++ backend/src/main/java/com/devSuperior/dsCatalog/entities/Product.java
@@ -33,7 +33,13 @@ public class Product implements Serializable {
   private Instant date;
 
   @ManyToMany
-  @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+  @JoinTable(
+  // @formatter:off
+    name = "tb_product_category", 
+    joinColumns = @JoinColumn(name = "product_id"), 
+    inverseJoinColumns = @JoinColumn(name = "category_id")
+    // @formatter:on
+  )
   Set<Category> categories = new HashSet<>();
 
   public Product() {
