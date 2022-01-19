23bdf86d5f0dac39cf84d11a3f00029c3bda45ce
venilson1 <venilson2009@hotmail.com>
2022-01-19 19:59:02 -0300

    basic bean validation an annotation

--- backend/src/main/java/com/devSuperior/dsCatalog/resources/ProductResouce.java
+++ backend/src/main/java/com/devSuperior/dsCatalog/resources/ProductResouce.java
@@ -2,6 +2,8 @@ package com.devSuperior.dsCatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import com.devSuperior.dsCatalog.dto.ProductDTO;
 import com.devSuperior.dsCatalog.services.ProductService;
 
@@ -40,14 +42,14 @@ public class ProductResouce {
   }
 
   @PostMapping
-  public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+  public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
     dto = service.insert(dto);
     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
     return ResponseEntity.created(uri).body(dto);
   }
 
   @PutMapping(value = "/{id}")
-  public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+  public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
     dto = service.update(id, dto);
     return ResponseEntity.ok().body(dto);
   }
--- backend/src/main/java/com/devSuperior/dsCatalog/resources/UserResouce.java
+++ backend/src/main/java/com/devSuperior/dsCatalog/resources/UserResouce.java
@@ -2,6 +2,8 @@ package com.devSuperior.dsCatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import com.devSuperior.dsCatalog.dto.UserDTO;
 import com.devSuperior.dsCatalog.entities.UserInsertDTO;
 import com.devSuperior.dsCatalog.services.UserService;
@@ -41,14 +43,14 @@ public class UserResouce {
   }
 
   @PostMapping
-  public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+  public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
     UserDTO newDto = service.insert(dto);
     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
     return ResponseEntity.created(uri).body(newDto);
   }
 
   @PutMapping(value = "/{id}")
-  public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
+  public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
     dto = service.update(id, dto);
     return ResponseEntity.ok().body(dto);
   }
