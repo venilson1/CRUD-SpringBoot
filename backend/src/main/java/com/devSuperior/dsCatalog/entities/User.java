dfd2926030d4454cdf79b73f47fca2466d16fa3f
venilson1 <venilson2009@hotmail.com>
2022-02-07 14:35:07 -0300

    implementing OAuth2 Authorization server

--- backend/src/main/java/com/devSuperior/dsCatalog/entities/User.java
+++ backend/src/main/java/com/devSuperior/dsCatalog/entities/User.java
@@ -1,8 +1,10 @@
 package com.devSuperior.dsCatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,11 +17,16 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
-
-  @Id
+public class User implements UserDetails, Serializable {
+	private static final long serialVersionUID = 1L;
+	
+@Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String firstName;
@@ -118,5 +125,36 @@ public class User implements Serializable {
       return false;
     return true;
   }
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 }
--- backend/src/main/java/com/devSuperior/dsCatalog/services/UserService.java
+++ backend/src/main/java/com/devSuperior/dsCatalog/services/UserService.java
@@ -17,17 +17,24 @@ import com.devSuperior.dsCatalog.repositories.UserRepository;
 import com.devSuperior.dsCatalog.services.exceptions.DatabaseException;
 import com.devSuperior.dsCatalog.services.exceptions.ResourceNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
   @Autowired
   private UserRepository repository;
@@ -102,4 +109,16 @@ public class UserService {
     }
 
   }
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		
+		if(user == null) {
+			logger.error("user not found: " + username);
+			throw new UsernameNotFoundException("Email not found!");
+		}
+		logger.info("user found: " + username);
+		return user;
+	}
 }
